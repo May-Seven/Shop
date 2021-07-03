9b84eaaa99a3f2419e4fb03eeed567ac0e238eb8
May-Seven <zhaopeijie@email.cn>
2021-07-04 00:04:50 +0800

    loginpage designer

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
 
-Vue.config.productionTip = false
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+//导入全局样式表
+import "./assets/css/global.css";
+import axios from 'axios'
+Vue.use(ElementUI);
+Vue.config.productionTip = false;
+
+axios.defaults.baseURL = "http://test.qhhz.xyz:8888/api/private/v1/";
+
+Vue.prototype.$http = axios;
+
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   render: h => h(App)
-})
+});
